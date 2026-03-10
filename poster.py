281c56c2312f6b64e01d73d499f439a2d24ef7b3
ProgSaif <isaifulone@gmail.com>
2026-03-11 00:44:15 +0600

    Update poster.py

--- poster.py
+++ poster.py
@@ -32,7 +32,7 @@ def generate_signal_message(coin, entry, sl, tp1, tp2, tp3, trade_type="LONG", c
     entry_high = entry * 1.005
 
     message = f"""
-💹 ${coin_name} — {trade_type}
+${coin_name} — {trade_type}
 
 Entry: {entry_low:.8f} – {entry_high:.8f}
 SL: {sl:.8f}
